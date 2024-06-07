5b5c11862ec5e4aec0b89c5d0c9d23672cf7eb97
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-06-07 12:33:32 -0600

    tab with domains, genomes, and 2nd neighbour

--- LXG_flag.R
+++ LXG_flag.R
@@ -24,23 +24,48 @@ NAMES <- c(
 
 
 
-iscan <- read_tsv(IN, col_names = NAMES)
+iscan <- read_tsv(IN, col_names = NAMES, na = c("-", "NA", ""))
 
-iscan <- iscan |>
+iscan_lxg <- iscan |>
   filter(interpro == LXG)
 
-iscan <- iscan |>
+iscan_lxg <- iscan_lxg |>
   select(pid, interpro, interpro_txt) |>
   distinct()
 
-iscan$LXG <- TRUE
+iscan_lxg$LXG <- TRUE
 
-neighW<- read_tsv("second_from_endoV.tsv")
+neighW <- read_tsv("second_from_endoV.tsv")
 
 
-out <- left_join(neighW, iscan, join_by(pid == pid)) |>
+out <- left_join(neighW, iscan_lxg, join_by(pid == pid)) |>
   relocate(pid, LXG)
 
 out$LXG[is.na(out$LXG)] <- FALSE
 
-write_tsv(out, "LXG_on_second.tsv")
+# write_tsv(out, "LXG_on_second.tsv")
+
+
+# find other IPR_IDS ------------------------------------------------------
+
+library(stringr)
+
+rm_na <- function(x) {
+  x[!is.na(x)]
+}
+
+
+collapse_ipr <- function(x) {
+  str_flatten(
+    unique(rm_na(x)),
+    collapse = ";"
+  )
+}
+
+
+pids_doms <- iscan |>
+  select(pid, interpro) |>
+  group_by(pid) |>
+  summarize(domains = collapse_ipr(interpro))
+
+# write_tsv(pids_doms, "pids_doms.tsv")
--- singleton_flag.R
+++ singleton_flag.R
@@ -23,12 +23,12 @@ OUT <- left_join(lxg, flag, join_by(genome == genome)) |>
   select(-n, -singleton) |>
   rename(from_endoV = Nseq)
 
-view(OUT)
+# view(OUT)
 
 IN_all_bsub <- "1423_refseq.tsv"
 all_1423 <- read_tsv(IN_all_bsub)
 
-SEL_1423 <- c("genome", "org", "tax_id", "level" ,"date",  "owner", "proj",  "n_cds" ,"completeness")
+SEL_1423 <- c("genome", "org", "tax_id", "level", "date", "owner", "proj", "n_cds", "completeness")
 
 all_1423 <- all_1423 |>
   select(all_of(SEL_1423))
@@ -46,8 +46,22 @@ OUT2 <- left_join(all_1423, OUT, join_by(genome == genome)) |>
 
 OUT2 <- OUT2 |>
   arrange(desc(is.na(pid)), LXG, blast_pair)
-view(OUT2)
+# view(OUT2)
 
 # WTFFFFF!!!!!!!!!!!!
 # GCF_018603715.1
 # WP_003242882.1
+
+
+IN_SEQ <- "2nd_pids.tsv"
+seqs <- read_tsv(IN_SEQ, col_names = c("pid", "seq"))
+
+OUT2 <- left_join(OUT2, seqs, join_by(pid == pid))
+
+
+doms <- read_tsv("pids_doms.tsv")
+
+OUT2 <- left_join(OUT2, doms, join_by(pid == pid))
+
+
+# write_tsv(OUT2, "Bsub_2ndFromEndoV.tsv")
